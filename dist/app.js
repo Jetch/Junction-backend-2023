f51f9af096383b52e4977c8901156d10df445ee7
Rich <applerich0306@gmail.com>
2023-11-11 21:52:13 +0200

    change

--- dist/app.js
+++ dist/app.js
@@ -275,7 +275,9 @@ function handleReceivedSensorData(data) {
     calculateAbsoluteDifference(data);
 }
 
-socket.on('motion', motionData => {
+    var socket = null;
+    socket = io();
+    socket.on('motion', motionData => {
     handleReceivedSensorData(motionData);
     console.log(motionData)
 });
