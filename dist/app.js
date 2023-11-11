a186a8eb614b49ed55905397589066b12f600976
Rich <applerich0306@gmail.com>
2023-11-11 22:10:15 +0200

    change

--- dist/app.js
+++ dist/app.js
@@ -203,21 +203,22 @@ Skeleton.prototype.display = function () {
 }
 
 //
-let prevSensorData = null;
 
-const movementThreshold = 0.1;
+// const movementThreshold = 0.1;
+// const initialSliderValue = 5;
 
-const initialSliderValue = 5;
+// const audio1Slider = document.getElementById("myRange1");
+// audio1Slider.value = initialSliderValue;
 
-const audio1Slider = document.getElementById("myRange1");
-audio1Slider.value = initialSliderValue;
+// const audio2Slider = document.getElementById("myRange2");
+// audio2Slider.value = initialSliderValue;
 
-const audio2Slider = document.getElementById("myRange2");
-audio2Slider.value = initialSliderValue;
+// function updateSliderValue(value) {
+//     audio1Slider.value = value;
+//     // Rest of your code
+// }
+    
 
-function updateSliderValue(value) {
-    audio1Slider.value = value;
-}
 
 function calculateAbsoluteDifference(data) {
     if (prevSensorData) {
@@ -229,57 +230,41 @@ function calculateAbsoluteDifference(data) {
 
         if (diffAx >= 2) {
             console.log('Big Movement on _A_ Detected!');
-            const newValue = parseInt(audio1Slider.value) + 1;
-            updateSliderValue(newValue);
-            console.log(audio1Slider.value);
-            //can also add the drum
-            const keyEvent = new KeyboardEvent('keydown', {
-                key: 'f'
-            });
-            document.dispatchEvent(keyEvent);
+            // const newValue = parseInt(audio1Slider.value) + 1;
+            // updateSliderValue(newValue);
+            // console.log(audio1Slider.value);
+            // //can also add the drum
+            // const keyEvent = new KeyboardEvent('keydown', {
+            //     key: 'f'
+            // });
+            // document.dispatchEvent(keyEvent);
 
-            const keyUp = new KeyboardEvent('keyup', {
-                key: 'f',
-            });
-            document.dispatchEvent(keyUp);
+            // const keyUp = new KeyboardEvent('keyup', {
+            //     key: 'f',
+            // });
+            // document.dispatchEvent(keyUp);
             Skeleton.dancer.raiseShoulder();
         }
 
         if (Math.abs(data.ax) < 0.03) 
         {
             console.log('Move!!!!!');
-            const keyEvent = new KeyboardEvent('keydown', {
-                key: 'g',
-            });
-            document.dispatchEvent(keyEvent);
+            // const keyEvent = new KeyboardEvent('keydown', {
+            //     key: 'g',
+            // });
+            // document.dispatchEvent(keyEvent);
 
-            const keyUp = new KeyboardEvent('keyup', {
-                key: 'g',
-            });
-            document.dispatchEvent(keyUp);
+            // const keyUp = new KeyboardEvent('keyup', {
+            //     key: 'g',
+            // });
+            // document.dispatchEvent(keyUp);
             
-            const newValue = parseInt(audio1Slider.value) - 1;
-            updateSliderValue(newValue);
+            // const newValue = parseInt(audio1Slider.value) - 1;
+            // updateSliderValue(newValue);
         }
 
         if (diffAx >= 3) {
-            console.log('Big Movement on _O_ Detected!');
-            
-            const mouseClickEvent = new MouseEvent('mousedown', {
-                bubbles: true,
-                cancelable: true,
-                view: window
-            });
-
-            document.dispatchEvent(mouseClickEvent);
-
-            const mouseUp = new MouseEvent('mouseup', {
-                bubbles: true,
-                cancelable: true,
-                view: window
-            });
-
-            document.dispatchEvent(mouseUp);
+            console.log('Big Movement on _O_ Detected!');                    
         }
     }
 
@@ -290,8 +275,8 @@ function handleReceivedSensorData(data) {
     calculateAbsoluteDifference(data);
 }
 
-var socket = null;
-socket = io();
+// var socket = null;
+// socket = io();
 socket.on('motion', motionData => {
     handleReceivedSensorData(motionData);
     console.log(motionData)
